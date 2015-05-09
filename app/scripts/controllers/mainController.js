f31f3b0d4e850db5db742989de5e0cb93a798b1a
Sadi <inaveq@gmail.com>
2015-05-09 13:46:39 +0900

    init commit-m

---
+++ app/scripts/controllers/mainController.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name Conceptum.controller:MainController
+ * @description
+ * # MainController
+ */
+angular.module('Conceptum')
+  .controller('MainController', function($scope) {
+
+    // do something with $scope
+
+  });
---
+++ app/scripts/controllers/settingsController.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name Conceptum.controller:SettingsController
+ * @description
+ * # SettingsController
+ */
+angular.module('Conceptum')
+  .controller('SettingsController', function($scope) {
+
+    // do something with $scope
+
+  });
